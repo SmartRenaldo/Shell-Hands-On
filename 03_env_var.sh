09873019b796d034d706e75b81136fccf5f44150
SmartRenaldo <correctgavin@gmail.com>
2022-05-16 21:56:34 +0930

    submit 03 & 04

---
+++ 03_env_var.sh
@@ -0,0 +1,5 @@
+#use env var TOMCAT_HOME
+echo "TOMCAT_HOME=$TOMCAT_HOME"
+:<<!
+	I am multi-line comment!
+!
---
+++ 04_pos_var.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "$0 $1 $2"
+echo "all args=$*"
+echo "all args=$@"
+echo "var num=$#"
